c8231427590cedcbd7c9f0e021de7cc8bef8f84e
alphajuliet <andrew@alphajuliet.com>
2026-08-09 20:19:39 +1000

    Solve q01_p3.

--- src/q01.clj
+++ src/q01.clj
@@ -19,6 +19,11 @@
   [a b n]
   (-> n (max a) (min b)))
 
+(defn swap-elements
+  "Swap two elements of a collection"
+  [coll i j]
+  (assoc coll j (nth coll i) i (nth coll j)))
+
 (defn part1
   "Solution for part 1"
   [fname]
@@ -41,24 +46,46 @@
         index (reduce (fn [acc e]
                         (let [dir (first e)
                               x (Integer/parseInt (subs e 1))
-                              n (dec (count words))]
+                              n (count words)]
                           (case dir
-                            \L (mod n (- acc x))
-                            \R (mod n (+ acc x)))))
+                            \L (mod (- acc x) n)
+                            \R (mod (+ acc x) n))))
                       0
                       actions)] 
     (nth words index)))
+
+(defn part3
+  "Solution for part 3"
+  [fname]
+  (let [[words actions] (read-data fname)
+        n (count words)
+        indices (reduce (fn [acc e]
+                          (let [dir (first e)
+                                x (Integer/parseInt (subs e 1))]
+                            (case dir
+                              \L (swap-elements acc 0 (mod (- x) n))
+                              \R (swap-elements acc 0 (mod x n)))))
+                        (vec (range n))
+                        actions)] 
+    (nth words (first indices))))
 
 (comment
   (def testf1 "data/q01_p1_test.txt")
   (def inputf1 "data/q01_p1.txt")
 
-  (def testf2 "data/q01_p2_test.txt")
+  (def testf2 "data/q01_p1_test.txt")
   (def inputf2 "data/q01_p2.txt")
+
+  (def testf3 "data/q01_p3_test.txt")
+  (def inputf3 "data/q01_p3.txt")
 
   (part1 testf1)
   (part1 inputf1)
 
   (part2 testf2)
-  (part2 inputf2))
+  (part2 inputf2)
+
+  (part3 testf3)
+  (part3 inputf3))
+
 ;; The End
