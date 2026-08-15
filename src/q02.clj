bdcc8cc04936d78c006935d2b7656fc039751ac3
alphajuliet <andrew@alphajuliet.com>
2026-08-15 17:06:06 +1000

    Solve q02_p2.

--- src/q02.clj
+++ src/q02.clj
@@ -1,17 +1,16 @@
-(ns q02
-  (:require [clojure.string :as str]
-            [util :as util]))
+(ns q02)
 
 (defn read-data
   [f]
   (->> f
        slurp
-       (re-seq #"(\d+),(\d+)")
+       (re-seq #"A=\[(-?\d+),(-?\d+)\]")
        first
        rest
-       (map #(Integer/parseInt %))))
+       (mapv #(Integer/parseInt %))))
 
 (defn c-add
+  "Complex add"
   [z1 z2]
   (mapv + z1 z2))
 
@@ -21,31 +20,54 @@
   [(- (* x1 x2) (* y1 y2)) (+ (* x1 y2) (* y1 x2))])
 
 (defn c-div
+  "Scalar division"
   [z1 z2]
   (map quot z1 z2))
 
-(defn a-cycle
+(defn cycle-once
   "Run one cycle"
-  [r a]
+  [r a d]
   (-> r
       (c-mul r)
-      (c-div [10 10])
+      (c-div d)
       (c-add a)))
+
+(defn out-of-bounds?
+  [[x y]]
+  (or (> (abs x) 1000000)
+      (> (abs y) 1000000)))
+
+(defn engrave?
+  "Does this point get engraved?"
+  [z]
+  (let [x (reduce
+            (fn [acc _]
+              (if (out-of-bounds? acc)
+                (reduced [1000001 1000001])
+                (cycle-once acc z [100000 100000])))
+            [0 0]
+            (range 100))]
+    (not (out-of-bounds? x))))
 
 (defn part1
   "Solution for part 1"
   [fname]
   (let [a (read-data fname)]
     (reduce 
-      (fn [acc _]
-        (a-cycle acc a))
+      (fn [acc _] (cycle-once acc a [10 10]))
       [0 0]
       (range 3))))
-  
+
 (defn part2
   "Solution for part 2"
-  [fname])
-  
+  [fname]
+  (let [a (read-data fname)
+        m (for [i (range 101)
+                j (range 101)]
+            (c-add a [(* i 10) (* j 10)]))]
+    (->> m
+         (filter engrave?)
+         count)))
 
 (comment
   (def testf1 "data/q02_p1_test.txt")
@@ -59,5 +81,6 @@
 
   (part2 testf2)
   (part2 inputf2))
+  ;; not 1299
 
 ;; The End
