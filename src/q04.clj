b37640be7cb755d7ed9abb69f93fc8846b00bdad
alphajuliet <andrew@alphajuliet.com>
2026-08-22 11:45:23 +1000

    Solve q04_p3.

--- src/q04.clj
+++ src/q04.clj
@@ -9,6 +9,16 @@
        str/split-lines
        (map Integer/parseInt)))
 
+(defn read-data3
+  [f]
+  (let [lines (->> f slurp str/split-lines)]
+    (->> lines
+         (map #(str/split % #"\|"))
+         flatten
+         (map Integer/parseInt)
+         (partition 2)
+         (map (partial apply /)))))
+
 (defn part1
   "Solution for part 1"
   [fname]
@@ -23,20 +33,33 @@
   "Solution for part 2"
   [fname]
   (let [coll (read-data fname)
-         lcm (apply util/lcm coll)
-         [a b] ((juxt first last) (map (partial / lcm) coll))
-         r (/ b a)]
+        lcm (apply util/lcm coll)
+        [a b] ((juxt first last) (map (partial / lcm) coll))
+        r (/ b a)]
     (inc (bigint (/ 10000000000000N r)))))
+
+(defn part3
+  [fname]
+  (let [coll (read-data3 fname)]
+    (->> coll
+         (apply *)
+         (* 100)
+         bigint)))
 
 (comment
   (def testf1 "data/q04_p1_test2.txt")
   (def inputf1 "data/q04_p1.txt")
   (def testf2 "data/q04_p1_test2.txt")
   (def inputf2 "data/q04_p2.txt")
+  (def testf3 "data/q04_p3_test.txt")
+  (def inputf3 "data/q04_p3.txt")
 
   (part1 testf1)
   (part1 inputf1)
 
   (part2 testf2)
-  (part2 inputf2))
+  (part2 inputf2)
+ 
+  (part3 testf3)
+  (part3 inputf3))
 ;; The End
