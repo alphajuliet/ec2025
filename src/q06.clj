c8f196b4fffa902497de07d2f2911cb808c4bcb2
alphajuliet <andrew@alphajuliet.com>
2026-08-23 13:27:26 +1000

    Solve q06_p2.

--- src/q06.clj
+++ src/q06.clj
@@ -3,22 +3,45 @@
             [util :as util]))
 
 (defn read-data
-  [f])
+  [f]
+  (->> f
+       slurp
+       str/trim-newline))
+
+(defn count-x-before-y 
+  "Count the occurrences of x before y in a string s"
+  [x y s]
+  (->> s
+       (reduce 
+         (fn [[total x-count] ch]
+           (condp = ch
+             x [total (inc x-count)]
+             y [(+ total x-count) x-count]
+             [total x-count]))
+         [0 0])
+       first))
 
 (defn part1
   "Solution for part 1"
-  [fname])
+  [fname]
+  (->> fname
+       read-data
+       (count-x-before-y \A \a)))
   
 
 (defn part2
   "Solution for part 2"
-  [fname])
+  [fname]
+  (let [s (read-data fname)
+        a (count-x-before-y \A \a s)
+        b (count-x-before-y \B \b s)
+        c (count-x-before-y \C \c s)]
+    (+ a b c)))
   
-
 (comment
   (def testf1 "data/q06_p1_test.txt")
   (def inputf1 "data/q06_p1.txt")
-  (def testf2 "data/q06_p2_test.txt")
+  (def testf2 "data/q06_p1_test.txt")
   (def inputf2 "data/q06_p2.txt")
 
   (part1 testf1)
