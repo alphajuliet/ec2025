6202220159229320c9e937e70f6ccfd529f405d2
alphajuliet <andrew@alphajuliet.com>
2026-08-23 13:15:22 +1000

    Solve q06_p1.

---
+++ src/q06.clj
@@ -0,0 +1,29 @@
+(ns q06
+  (:require [clojure.string :as str]
+            [util :as util]))
+
+(defn read-data
+  [f])
+
+(defn part1
+  "Solution for part 1"
+  [fname])
+  
+
+(defn part2
+  "Solution for part 2"
+  [fname])
+  
+
+(comment
+  (def testf1 "data/q06_p1_test.txt")
+  (def inputf1 "data/q06_p1.txt")
+  (def testf2 "data/q06_p2_test.txt")
+  (def inputf2 "data/q06_p2.txt")
+
+  (part1 testf1)
+  (part1 inputf1)
+
+  (part2 testf2)
+  (part2 inputf2))
+;; The End
