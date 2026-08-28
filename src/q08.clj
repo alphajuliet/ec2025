a5e1adc53cfd1c5c5a8d82d42c711eba544a8a69
alphajuliet <andrew@alphajuliet.com>
2026-08-28 20:53:24 +1000

    Solve q08_p2.

--- src/q08.clj
+++ src/q08.clj
@@ -1,5 +1,6 @@
 (ns q08
   (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]
             [util :as util]))
 
 (defn read-data
@@ -10,19 +11,37 @@
       (str/split #",")
       (#(map Integer/parseInt %))))
 
+(defn xor
+  [a b]
+  (or (and a (not b))
+      (and b (not a))))
+
+(defn intersects?
+  "Do the chords between the pairs of points intersect?"
+  [[a b] [c d]]
+  (if (or (= b c) (= a d) (= a c) (= b d))
+    false
+    (let [[lo hi] ((juxt min max) a b)]
+      (xor (< lo c hi) (< lo d hi)))))
+
 (defn part1
   "Solution for part 1"
   [fname n]
-  (let [v (read-data fname)]
-    (->> v
-         (map #(mod % (/ n 2)))
-         (partition 2 1)
-         (filter #(= (first %) (second %)))
-         count)))
+  (->> fname
+       read-data
+       (map #(mod % (/ n 2)))
+       (partition 2 1)
+       (filter #(= (first %) (second %)))
+       count))
 
 (defn part2
   "Solution for part 2"
-  [fname])
+  [fname]
+  (let [t (->> fname read-data (partition 2 1))
+        pairs (combo/combinations t 2)]
+    (->> pairs
+        (filter #(apply intersects? %))
+        count)))
 
 (comment
   (def testf1 "data/q08_p1_test.txt")
@@ -35,4 +54,5 @@
 
   (part2 testf2)
   (part2 inputf2))
+
 ;; The End
